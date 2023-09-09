f7aed22adb397cb5cdb555b2edcc2523cfaa753c
Melekh11 <cktkutxd@gmail.com>
2023-09-09 16:18:41 +0300

    cors

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,21 @@ from routes.users import user_router
 from routes.todos import todo_router
 from routes.tags import tag_router
 from database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ["*"]
 
 Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user_router)
 app.include_router(todo_router)
 app.include_router(tag_router)
